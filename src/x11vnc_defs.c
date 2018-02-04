b6eeddff17b50c5129a294dd058fe0a88a44ca61
Christian Beier <dontmind@freeshell.org>
2018-02-04 22:35:28 +0100

    Update lastmod

--- src/x11vnc_defs.c
+++ src/x11vnc_defs.c
@@ -49,7 +49,7 @@ int xtrap_base_event_type = 0;
 int xdamage_base_event_type = 0;
 
 /*               date +'lastmod: %Y-%m-%d' */
-char lastmod[] = VERSION " lastmod: 2015-11-14";
+char lastmod[] = VERSION " lastmod: 2018-02-04";
 
 /* X display info */
 
