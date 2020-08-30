7ea0b9094c20c2145c1d964f07ee475a74399e97
Keith Bennett <inetknght@knightoftheinter.net>
2020-08-30 16:06:30 -0500

    per review and rfc, limit password to 8 bytes

--- src/x11vnc.c
+++ src/x11vnc.c
@@ -1745,18 +1745,10 @@ static void check_appshare_mode(int argc, char* argv[]) {
 
 static void store_homedir_passwd(char *file) {
 	char str1[32], str2[32], *p, *h, *f;
-	int newlines, maxlen;
 	struct stat sbuf;
 
 	str1[0] = '\0';
 	str2[0] = '\0';
-	/* fgets() stops at and includes the newline */
-	/* unless the input limit was reached, then it stops at the limit */
-	/* and does _not_ include the newline. trailing null is always set. */
-	newlines = 0;
-	/* maxlen excludes trailing null and newline from input */
-	maxlen = (sizeof(str1)/sizeof(char)) - 2;
-
 	/* storepasswd */
 	if (no_external_cmds || !cmd_ok("storepasswd")) {
 		fprintf(stderr, "-nocmds cannot be used with -storepasswd\n");
@@ -1782,16 +1774,15 @@ static void store_homedir_passwd(char *file) {
 	system("stty echo");
 
 	if ((p = strchr(str1, '\n')) != NULL) {
-		++newlines;
 		*p = '\0';
 	}
 	if ((p = strchr(str2, '\n')) != NULL) {
-		++newlines;
 		*p = '\0';
 	}
-	if (2 != newlines) {
-		/* this is just for a clearer error message. strcmp() will also fail */
-		fprintf(stderr, "** password exceeds maximum %d bytes.\n", maxlen);
+	if (8 < strlen(str1)) {
+		/* RFC6143 states: "the password is truncated to eight characters" */
+		/* there's room for ambiguity (characters vs bytes) */
+		fprintf(stderr, "** password exceeds maximum 8 bytes.\n");
 		exit(1);
 	}
 	if (strcmp(str1, str2)) {
