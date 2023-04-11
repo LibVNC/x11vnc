179b115ec51970af26cb842fb27058c705511a00
orbea <orbea@riseup.net>
2023-04-11 09:35:26 -0700

    sslhelper: Update for LibreSSL >= 3.6.0
    Since LibreSSL 3.6.0 SSL_CTX_set_security_level() has been available.

--- src/sslhelper.c
+++ src/sslhelper.c
@@ -1596,7 +1596,8 @@ static int switch_to_anon_dh(void) {
 	if (ssl_client_mode) {
 		return 1;
 	}
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)) || \
+	(defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x3060000fL)
 	/* Security level must be set to 0 for unauthenticated suites. */
 	SSL_CTX_set_security_level(ctx, 0);
 #endif
